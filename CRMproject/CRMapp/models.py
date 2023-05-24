e25296b8988a76874059f94ca88485bcee2fc471
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-24 12:11:30 +0300

    1234

--- CRMproject/CRMapp/models.py
+++ CRMproject/CRMapp/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
