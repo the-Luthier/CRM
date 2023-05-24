93964182e802736441938534f08bf334d61c30e5
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-24 21:18:30 +0300

    1234

--- CRMproject/CRMapp/admin.py
+++ CRMproject/CRMapp/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.admin.sites import NotRegistered
+from .models import Profile, Device, FileError, Notifications, Subscriptions
+from .utils import send_push_notifications
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Device)
+admin.site.register(FileError)
+admin.site.register(Notifications)
+admin.site.register(Subscriptions)
+
+
+def send_notifications(modeladmin, request, queryset):
+    profile_ids = queryset.values_list('id', flat=True)
+    device_tokens = Device.objects.filter(profile__in=profile_ids).values_list('device_token', flat=True)
+    
+    title = "Your notification title"
+    message = "Your notification message"
+    
+    send_push_notifications(device_tokens, title, message)
+
+send_notifications.short_description = "Send notifications to selected profiles"
+
+class CustomProfileAdmin(admin.ModelAdmin):
+    actions = [send_notifications]
+
+# Unregister the Profile model if it's already registered
+try:
+    admin.site.unregister(Profile)
+except NotRegistered:
+    pass
+
+# Register the Profile model with the custom admin class
+admin.site.register(Profile, CustomProfileAdmin)
