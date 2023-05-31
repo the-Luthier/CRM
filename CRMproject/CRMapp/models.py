cb980c8af3eb93532b0e2102bd4f173c3ff21c50
Onur KAVRIK <80355788+the-Luthier@users.noreply.github.com>
2023-05-31 15:47:04 +0300

    1234

--- CRMproject/CRMapp/models.py
+++ CRMproject/CRMapp/models.py
@@ -15,8 +15,12 @@ class ProfileTypes(models.TextChoices):
 
 
 class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=False)
+    id = models.AutoField(primary_key=True)
+    verification_code = models.CharField(max_length=6, blank=False)
+    full_name = models.CharField(max_length=200, blank=False)
 
-    REQUIRED_FIELDS = ['phone_number', 'full_name', 'adress', 'verification_code', 'is_verified', 'date_of_birth',]
+    REQUIRED_FIELDS = ['phone_number', 'id', 'adress', 'verification_code', 'is_verified', 'date_of_birth',]
 
 
 class Profile(models.Model):
@@ -34,7 +38,9 @@ class Profile(models.Model):
     date_of_birth = models.DateField()
 
     def __str__(self):
-            return f'{self.user.username}\'s profile'    
+            return f'{self.user.username}\'s profile'   
+ 
+
 
 
 class FileError(models.Model):
@@ -74,7 +80,7 @@ class Subscriptions(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.MultipleObjectsReturned('title', 'description', 'created_at', 'user',)
+        return self.MultipleObjectsReturned('title', 'description', 'created_at', 'user', 'price')
     
 
 class Device(models.Model):
